52257a5557be7cf3543df93f9a62fffdad2e9dd7
andreydedey <dedey.cardoso@gmail.com>
2026-05-10 19:59:18 -0300

    api

---
+++ server/saleSheet/src/test/java/com/example/salesheet/SaleSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleSheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
