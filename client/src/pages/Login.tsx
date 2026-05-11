4200961c19a4c15ce5e717bb9fe2468fdf0b2cc3
andreydedey <dedey.cardoso@gmail.com>
2026-05-11 19:33:19 -0300

    remove: dark theme

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+
 export const Login = () => {
-  return <div>Login</div>
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <h1 className="text-xl text-black">Bem-vindo de volta</h1>
+        </CardTitle>
+      </CardHeader>
+    </Card>
+  )
 }
