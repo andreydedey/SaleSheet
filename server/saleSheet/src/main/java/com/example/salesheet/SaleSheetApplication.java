4e4cccd3803401a910d84ddd74e55e32d4dfe258
andreydedey <dedey.cardoso@gmail.com>
2026-08-08 01:11:20 -0300

    fix: enable spring web support

--- server/saleSheet/src/main/java/com/example/salesheet/SaleSheetApplication.java
+++ server/saleSheet/src/main/java/com/example/salesheet/SaleSheetApplication.java
@@ -2,10 +2,14 @@ package com.example.salesheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableAsync
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class SaleSheetApplication {
 
     public static void main(String[] args) {
