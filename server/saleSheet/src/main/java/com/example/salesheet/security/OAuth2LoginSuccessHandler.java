4328ae5544e3f827745880fa56bf8b3ac82faaab
andreydedey <dedey.cardoso@gmail.com>
2026-08-10 16:44:21 -0300

    fix: backend hardcoded url's

--- server/saleSheet/src/main/java/com/example/salesheet/security/OAuth2LoginSuccessHandler.java
+++ server/saleSheet/src/main/java/com/example/salesheet/security/OAuth2LoginSuccessHandler.java
@@ -3,6 +3,7 @@ package com.example.salesheet.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,9 @@ import java.io.IOException;
 @Component
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
+    @Value("${app.url}")
+    private String appUrl;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
@@ -19,8 +23,8 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         CustomUserPrincipal principal = (CustomUserPrincipal) authentication.getPrincipal();
 
         String redirectUrl = switch (principal.getUser().getRole()) {
-            case ADMIN -> "http://127.0.0.1:5173/dashboard";
-            case SALESPERSON -> "http://127.0.0.1:5173/salesperson/home";
+            case ADMIN -> appUrl + "/dashboard";
+            case SALESPERSON -> appUrl + "/salesperson/home";
         };
 
         response.sendRedirect(redirectUrl);
